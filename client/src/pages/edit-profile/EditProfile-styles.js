d782dc62043bdda5652b5ef37fb5cb337f4eca1a
fflorezz <felipeflorezart@gmail.com>
2020-11-15 17:32:34 -0500

    Added editProfile form

---
+++ client/src/pages/edit-profile/EditProfile-styles.js
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+import * as styles from "../../global-styles";
+
+const StyledEditProfile = styled.div`
+  width: 100%;
+  padding: 2rem 6rem;
+  border-radius: 0.8rem;
+  display: inline-flex;
+  flex-direction: column;
+  background-color: ${styles.colors.white};
+  position: relative;
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    margin-bottom: 1rem;
+    label {
+      font: ${styles.font.p1};
+      color: ${styles.colorsDark.gray};
+      margin-bottom: 0.5rem;
+    }
+    input {
+      width: 60rem;
+      height: 4rem;
+      margin-bottom: 4rem;
+      padding-left: 1rem;
+      border: 1px solid ${styles.colors.gray};
+      border-radius: 0.9rem;
+      font: ${styles.font.p1};
+      color: ${styles.colors.black};
+      background-color: ${styles.colorsLight.gray};
+    }
+    textarea {
+      width: 60rem;
+      border: 1px solid ${styles.colors.gray};
+      border-radius: 0.9rem;
+      margin-bottom: 2rem;
+      resize: none;
+      padding: 1rem 1rem;
+      font: ${styles.font.p1};
+      color: ${styles.colors.black};
+      background-color: ${styles.colorsLight.gray};
+      border: 1px solid ${styles.colors.gray};
+    }
+  }
+  .alert-text {
+    font: ${styles.font.psm};
+    color: ${styles.colorsDark.primary};
+    margin-bottom: 1.5rem;
+  }
+  .error {
+    outline-color: ${styles.colorsDark.primary};
+    border-color: ${styles.colorsDark.primary};
+  }
+`;
+
+export default StyledEditProfile;
--- client/src/pages/edit-profile/EditProfilePage.jsx
+++ client/src/pages/edit-profile/EditProfilePage.jsx
@@ -1,10 +1,60 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { useSelector } from "react-redux";
+
+import StyledEditProfile from "./EditProfile-styles";
+import Button from "./../../components/shared/button/Button";
 
 const EditProfilePage = () => {
+  const { register, handleSubmit, errors } = useForm();
+  const session = useSelector(state => state.session);
+
+  function onSubmit(data) {
+    console.log(data);
+  }
+
   return (
-    <div>
-      <h1>EDIT PROFILE</h1>
-    </div>
+    <StyledEditProfile>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="file"
+          style={{ display: "none" }}
+          accept=".jpg,.png,jpeg"
+          // ref={mergeRefs(filePickeRef, register)}
+          name="image"
+          // onChange={pickedHandler}
+        />
+        <label htmlFor="name">Nombre</label>
+        <input
+          defaultValue={session.userName}
+          type="text"
+          name="userName"
+          ref={register({ required: true, minLength: 5 })}
+          className={errors.name && "error"}
+        />
+        {errors.name && (
+          <p className="alert-text">
+            Es necesario un nombre de mínimo 5 caracteres
+          </p>
+        )}
+
+        <label htmlFor="password">Bio</label>
+        <textarea
+          name="description"
+          id="description"
+          cols="30"
+          rows="3"
+          ref={register({ required: true, minLength: 15, maxLength: 300 })}
+          className={errors.description && "error"}
+        ></textarea>
+        {errors.description && (
+          <p className="alert-text">
+            Es necesaria una descripción entre 15 y 120 caracteres
+          </p>
+        )}
+        <Button size="sm" text="Guardar" color="complementary" />
+      </form>
+    </StyledEditProfile>
   );
 };
 
